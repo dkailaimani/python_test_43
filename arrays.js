d984a233871015bec0555852f6d53f82055912c7
dkailaimani <102199241+dkailaimani@users.noreply.github.com>
2024-06-12 19:33:56 -0500

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,11 @@
+// Task 1: Adding and Removing Elements
+let fruits = ['apple', 'banana', 'orange'];
+
+// Add a new element to the end of the array
+fruits.push('grape');
+console.log("After adding:", fruits);
+
+// Remove the last element from the array
+let removedFruit = fruits.pop();
+console.log("After removing:", fruits);
+console.log("Removed fruit:", removedFruit);
